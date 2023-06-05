e37c2ec910475b4fd135a3731f14955ccddda195
Mike Wilson <mikewilsonstl@gmail.com>
2023-06-05 16:36:28 -0500

    Achieved MVP, added CSS, added banner, favicon, and navbar logo. Reduced brightness of background. Added CSS animation to navbar logo.

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,23 +1,36 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+    @Autowired
+    public EmployerRepository employerRepository;
+    @Autowired
+    public JobRepository jobRepository;
+    @Autowired
+    public SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
-
         model.addAttribute("title", "My Jobs");
 
         return "index";
@@ -27,26 +40,58 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+
+        Iterable<Employer> employers = employerRepository.findAll();
+        model.addAttribute("employers", employers);
+
+        Iterable<Skill> skills = skillRepository.findAll();
+        model.addAttribute("skills", skills);
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
                                        Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+        if(skills == null){
+            return "redirect:./add";
+        }
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-
-        return "redirect:";
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = optEmployer.get();
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
+            return "redirect:";
+        }
+        else{
+            return "redirect:";
+        }
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
+        Optional<Job> optJob = jobRepository.findById(jobId);
 
-        return "view";
+        if (optJob.isPresent()) {
+            Job job = optJob.get();
+            model.addAttribute("job", job);
+
+            return "view";
+        }
+        else{
+            return "redirect:..";
+        }
     }
 
-
+    @GetMapping
+    public String displayHomePage(Model model){
+        model.addAttribute("jobs", jobRepository.findAll());
+        return "index";
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,37 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Invalid location. Cannot be blank.")
+    @Size(min = 3, max = 50, message = "Invalid location. Must be between 3 and 50 characters.")
+    private String location;
 
+    @OneToMany()
+    @JoinColumn(name = "employer_id")
+    public List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){
+
+    }
 }
